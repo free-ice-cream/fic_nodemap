b5d2cead1bdccacac6bcd3c1cb5f058a1cacd009
Joe Davis <j.davis@mac.com>
2018-03-16 16:21:53 +0000

    Sample JS file for api_key variable

---
+++ api_key.sample.js
@@ -0,0 +1,3 @@
+// API key, for accessing the data
+// Note: enter the API key below, rename or copy this file to 'api_key.js' and don't commit THAT file to GIT
+var api_key = "{API KEY GOES HERE}";
